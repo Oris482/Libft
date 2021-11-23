95e3aa148c8dacb3ab53239003de0d3ffa27c790
jaesjeon <jaesjeon@student.42seoul.kr>
2021-11-23 13:32:45 +0900

    Libft Part2 Finishedgit add ., needed to remake Makefile to contain bonus

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jaesjeon <jaesjeon@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 10:28:13 by jaesjeon          #+#    #+#             */
-/*   Updated: 2021/11/22 16:25:59 by jaesjeon         ###   ########.fr       */
+/*   Updated: 2021/11/23 13:30:54 by jaesjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,14 @@ static size_t	ft_digit(int n)
 
 static void	ft_putnum(char *result, int n, size_t digit, size_t end_p)
 {
+	char	*min;
+
+	min = "-2147483648";
+	if (n == -2147483648)
+	{
+		ft_strlcpy(result, min, 12);
+		return ;
+	}
 	while (digit >= end_p)
 	{
 		result[digit] = (n % 10) + 48;
@@ -44,9 +52,7 @@ char	*ft_itoa(int n)
 	size_t	digit;
 	size_t	end_p;
 	char	*result;
-	char	*min;
 
-	min = "-2147483648";
 	digit = ft_digit(n);
 	if (n < 0)
 		digit++;
@@ -59,9 +65,8 @@ char	*ft_itoa(int n)
 	{
 		end_p = 1;
 		result[0] = '-';
-		if (n == -2147483648)
-			return (min);
-		n *= (-1);
+		if (n != -2147483648)
+			n *= (-1);
 	}
 	ft_putnum(result, n, digit, end_p);
 	return (result);
